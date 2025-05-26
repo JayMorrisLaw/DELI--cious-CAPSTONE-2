9aa7f90221fc1bfa730f3fc5778ab6c0cd1dfe88
JayMorrisLaw <jaypoltergeist@gmail.com>
2025-05-26 13:31:55 -0400

    drinks class

--- DELI-ciousSandwichShop/src/main/java/com/pluralsight/Drinks.java
+++ DELI-ciousSandwichShop/src/main/java/com/pluralsight/Drinks.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
 public class Drinks {
+    private String size;
+    private String flavor;
+// constructor
+    public Drinks(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+    // setting prices
+    public double getPrice() {
+        if (size.equalsIgnoreCase("small")) {
+            return 2.00;
+        } else if (size.equalsIgnoreCase("medium")) {
+            return 2.50;
+        } else if (size.equalsIgnoreCase("large"))
+            return 3.00;
+          else {
+    return 0.0; 
+        }
+    }
+    //methods
+    public String getSize(){
+        return size;
+    }
+    public String getFlavor(){
+        return flavor;
+    }
 }
